5c3b77ca159f61ca6d2a87d0acdea95566c931be
Salem Sayed <salemsayed981@gmail.com>
2023-05-27 02:53:38 +0300

    Updates

---
+++ precache-manifest.24f116d19c49e47577a4ca8fa92ee231.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbbeaca12b6bf263ac532c5876b0179",
+    "url": "/cart/index.html"
+  },
+  {
+    "revision": "5f23cd44089f815f1764",
+    "url": "/cart/static/css/2.e396ebae.chunk.css"
+  },
+  {
+    "revision": "026ac3fe7cb42a6f21ae",
+    "url": "/cart/static/css/main.3db79c06.chunk.css"
+  },
+  {
+    "revision": "5f23cd44089f815f1764",
+    "url": "/cart/static/js/2.3d83c77b.chunk.js"
+  },
+  {
+    "revision": "026ac3fe7cb42a6f21ae",
+    "url": "/cart/static/js/main.67b58338.chunk.js"
+  },
+  {
+    "revision": "f6812af01361f9d61f0d",
+    "url": "/cart/static/js/runtime~main.e07918fe.js"
+  },
+  {
+    "revision": "0ab3921d9b80975c5597432ab59f5d0a",
+    "url": "/cart/static/media/fa-brands-400.0ab3921d.ttf"
+  },
+  {
+    "revision": "8b0ddedbb27cbc9971c8667caa8a0cc1",
+    "url": "/cart/static/media/fa-brands-400.8b0ddedb.woff2"
+  },
+  {
+    "revision": "20206738b2bffb741b00200d5d3d6d20",
+    "url": "/cart/static/media/fa-regular-400.20206738.ttf"
+  },
+  {
+    "revision": "61f30b79daf5b31f0d254a31fba66158",
+    "url": "/cart/static/media/fa-regular-400.61f30b79.woff2"
+  },
+  {
+    "revision": "c64278386c2bbb5e293e11b94ca2f6d1",
+    "url": "/cart/static/media/fa-solid-900.c6427838.woff2"
+  },
+  {
+    "revision": "e2ceb83946c9e5fc7eab24453a03bffb",
+    "url": "/cart/static/media/fa-solid-900.e2ceb839.ttf"
+  },
+  {
+    "revision": "4bc58bc16bb05a05d3a47a4f7e143b75",
+    "url": "/cart/static/media/fa-v4compatibility.4bc58bc1.woff2"
+  },
+  {
+    "revision": "d20cedd7e254d4b58b721b6995ca52b4",
+    "url": "/cart/static/media/fa-v4compatibility.d20cedd7.ttf"
+  }
+]);
